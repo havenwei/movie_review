cfff641c010eab3246d1a35b8e8053f7e6cc2c6b
havenwei <havenwei82@163.com>
2017-03-30 11:21:42 +0800

    move form to partial,style form with bootstrap,replace form with simple_form bootstrap template

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,14 +17,20 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to movies_path
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
   end
 
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to movies_path, notice: "Update Success"
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
 
   def destroy
